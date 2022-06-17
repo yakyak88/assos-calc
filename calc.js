81c02b8d0b4fc8355ae3e07ac373203cb9b9bfc0
yakir <danielyakir01@gmail.com>
2022-06-17 18:19:07 +0300

    update

--- calc.js
+++ calc.js
@@ -1,7 +1,6 @@
 const inp = document.querySelector(".inp");
 const btn = document.querySelector(".btn");
 
-<<<<<<< HEAD
 const request = async function () {
     const currencies = await fetch(
         "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json"
@@ -31,27 +30,11 @@ const getPrice = async function () {
 };
 
 getPrice();
-// const name = function () {
-//     console.log(`my name is ${firstname}`);
-//     yak(firstname);
-// };
 
-// const yak = function () {
-//     const fullname = yakir;
-//     return fullname;
-// };
-
-// inp.addEventListener("keyup", function (e) {
-//     const val = e.target.value;
-//     console.log(val);
-// });
-=======
-btn.addEventListener('click',function(){
-    const price = inp.value*4.32*0.80 
-    document.querySelector(".newPrice").textContent= `${price} Shekels` 
-    document.body.style.backgroundColor= 'yellow' 
-    inp.value= " "
-    inp.placeholder  = `תודה אוסי המיסניקית `
-
-})
->>>>>>> e805a5d21a8846be1172257fc7870ccd2b4ecc51
+btn.addEventListener("click", function () {
+    const price = inp.value * 4.32 * 0.8;
+    document.querySelector(".newPrice").textContent = `${price} Shekels`;
+    document.body.style.backgroundColor = "yellow";
+    inp.value = " ";
+    inp.placeholder = `תודה אוסי המיסניקית `;
+});
